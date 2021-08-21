e560280a906ccd84182367101e186fdbf244332b
otaiga <karl.turner5@gmail.com>
2021-08-21 01:19:26 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      animation: {
+        "spin-slow": "scenario 40s linear infinite",
+        "slow-ping": "ping 2.5s cubic-bezier(0, 0, 0.2, 1) infinite",
+        "slow-pulse": "pulse-slow 2.5s cubic-bezier(0, 0, 0.2, 1) infinite",
+        scenario: "scenario 40s ease-in-out infinite",
+      },
+      keyframes: {
+        scenario: {
+          "0%, 100%": { transform: "rotate(-180deg)" },
+          "50%": { transform: "rotate(180deg)" },
+        },
+        "pulse-slow": {
+          "0%, 100%": {
+            opacity: 0.2,
+          },
+          "50%": {
+            opacity: 0.8,
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
