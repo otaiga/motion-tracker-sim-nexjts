6c2cbb6f9d2e274120f22ccebf6fb8b24079d769
otaiga <karl.turner5@gmail.com>
2021-08-21 10:50:30 +0100

    typo on next-config

--- next-config.js
+++
@@ -1,5 +0,0 @@
-const debug = process.env.NODE_ENV !== "production";
-
-module.exports = {
-  assetPrefix: !debug ? "/motion-tracker-sim-nexjts/" : "",
-};
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
   reactStrictMode: true,
-}
+  assetPrefix: !debug ? "/motion-tracker-sim-nexjts/" : "",
+};
