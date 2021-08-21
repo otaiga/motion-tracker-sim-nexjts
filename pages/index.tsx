82825c178fd374cf222e54eb5b6041acc72360d4
otaiga <karl.turner5@gmail.com>
2021-08-21 19:10:04 +0100

    changed to use svg and some styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,9 @@
 import React, { useState } from "react";
 import Head from "next/head";
-import Image from "next/image";
 import Timer from "../components/timer";
 
 const Home = () => {
   const [markerCount, setmarkerCount] = useState(0);
-  const [markers, setMarkers] = useState<({ [key: string]: string } | null)[]>(
-    []
-  );
-
-  const possibleValues = ["0", "1/4", "1/3", "1/2"];
-  const possibleDirections = ["right", "left"];
-
-  const createMarkers = () => {
-    const newMarkers = [];
-    for (let i = 0; i < markerCount; i++) {
-      const direction =
-        possibleDirections[
-          Math.floor(Math.random() * possibleDirections.length)
-        ];
-      const value1 =
-        possibleValues[Math.floor(Math.random() * possibleValues.length)];
-      const value2 =
-        possibleValues[Math.floor(Math.random() * possibleValues.length)];
-      newMarkers.push({ value1, value2, direction });
-    }
-    return newMarkers;
-  };
 
   return (
     <div className="bg-black h-screen mx-auto overflow-hidden relative">
@@ -37,25 +14,21 @@ const Home = () => {
       </Head>
 
       <main>
-        <div className="flex justify-center items-center">
-          <div className="top-80 md:top-0 relative animate-scenario">
+        <div className="flex flex-col items-center">
+          <div className="animate-scenario absolute lg:top-0 top-1/2">
             <img
-              src="./trackerImage.png"
+              src="./trackerImage.svg"
               alt="Picture of radar background"
-              className="z-0"
-              height="full"
+              className="z-0 h-96 w-96 lg:h-full lg:w-full"
             />
-            <div className="flex animate-slow-pulse">
-              {createMarkers().map((marker, index) => (
-                <div
-                  key={`${index}`}
-                  className={`bg-blue-200 border-blue-300 border-4 border-opacity-75 rounded-full h-12 w-12 md:h-24 md:w-24 absolute top-${marker.value1} ${marker.direction}-${marker.value2}`}
-                ></div>
-              ))}
-            </div>
+            {/* <div className="flex animate-slow-pulse absolute">
+              <div
+                className={`bg-blue-200 border-blue-300 border-4 border-opacity-75 rounded-full h-12 w-12 md:h-24 md:w-24`}
+              ></div>
+            </div> */}
           </div>
-          <div className="bg-white absolute h-48 bottom-0 sm:bottom-1/4 lg:-bottom-1/4 lg:h-full w-48 lg:w-1/2 rounded-full animate-slow-ping"></div>
-          <div className="flex bg-blue-400 text-white text-2xl md:text-5xl py-8 md:py-12 absolute bottom-0 h-1/4 md:h-1/3 w-full z-20 justify-around">
+          <div className="bg-white absolute top-1/2 h-96 w-96 lg:h-full lg:w-1/2 rounded-full animate-slow-ping"></div>
+          <div className="flex bg-blue-400 text-white text-2xl md:text-5xl py-8 absolute bottom-0 h-1/4 w-full z-20 justify-around">
             <div className="flex justify-around w-full font-bold text-xs sm:text-base md:text-2xl lg:text-5xl">
               <p className="mr-10 sm:mr-24 md:mr-48">F.E.M.S. 5.562.92</p>
               <p className="ml-10 sm:ml-24 md:ml-48">CX. 54/38.10</p>
