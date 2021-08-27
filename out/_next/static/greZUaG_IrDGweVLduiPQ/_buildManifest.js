29c77d1545feadece62960564e1ab93018b4dc5e
otaiga <karl.turner5@gmail.com>
2021-08-27 08:51:04 +0100

    Deploy Next.js to gh-pages

--- out/_next/static/greZUaG_IrDGweVLduiPQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/733-3974e03aa4a0d4039b93.js","static/chunks/pages/index-7a8560b72f09935a8961.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
